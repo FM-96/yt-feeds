5ed0265293de0e6b6128d4c2bb155da198def310
Felix Müllner <felix.muellner.96@gmail.com>
2018-08-01 19:44:26 +0200

    Implement basic getting playlist items

--- app/playlists/playlists.controller.js
+++ app/playlists/playlists.controller.js
@@ -1,12 +1,43 @@
+const youtube = require('../../youtube.js');
+
 module.exports = {
 	handle,
 };
 
-function handle(req, res) {
-	// TODO
+async function handle(req, res) {
 	const {playlistId, format} = req.params;
+
+	const allPlaylistItems = [];
+
+	let nextPageToken;
+
+	// TODO validate playlistId and format
+
+	try {
+		let requestCount = 1; // XXX
+		do {
+			console.log(`request ${requestCount++}`); // XXX
+			const ytRes = await youtube.playlistItems.list({
+				part: 'id,snippet',
+				playlistId,
+				maxResults: 50,
+				pageToken: nextPageToken,
+			});
+			allPlaylistItems.push(...ytRes.data.items);
+			nextPageToken = ytRes.data.nextPageToken;
+		} while (nextPageToken);
+	} catch (err) {
+		res.status(500).send({
+			success: false,
+			error: err.message,
+		});
+		return;
+	}
+
 	res.send({
+		success: true,
 		playlistId,
 		format,
+		allPlaylistItems,
 	});
 }
